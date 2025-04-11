4389a8c49671a798cdc49fe3b53e6ed35cf9fc9c
Chirag Agarwal <99432148+Chiragx07@users.noreply.github.com>
2025-04-11 15:52:09 +0530

    Added entity classes for Client, Portfolio, and Security

---
+++ src/main/java/com/wellsfargo/counselor/WellsFargoTask2Application.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.counselor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellsFargoTask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellsFargoTask2Application.class, args);
+    }
+}
+
